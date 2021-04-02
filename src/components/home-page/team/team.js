ec241de52d9bac6367b00f70d191c3a38f32d9a4
SOG-web <raheemolalekanusman84@gmail.com>
2021-04-02 20:15:57 +0100

    m

--- src/components/home-page/team/team.js
+++ src/components/home-page/team/team.js
@@ -51,19 +51,31 @@ function Team() {
           <img src={Tiff} alt="founder" className="img-core-team" />
           <h2 className="names">Raheem Olalekan Usman</h2>
           <p className="position">CEO, Founder and Lead Developer</p>
-          <div className="links"></div>
+          <div className="links">
+            <i className="fab fa-linkedin icon"></i>
+            <i className="fab fa-twitter icon"></i>
+            <i className="fab fa-github icon"></i>
+          </div>
         </div>
         <div className="core-team-member t-two">
           <img src={Tiff} alt="core-team" className="img-core-team" />
           <h2 className="names t-two-p">Tiffany Kisiero</h2>
           <p className="position t-two-p">Lead Designer</p>
-          <div className="links"></div>
+          <div className="links">
+            <i className="fab fa-linkedin icon"></i>
+            <i className="fab fa-twitter icon"></i>
+            <i className="fab fa-instagram icon"></i>
+          </div>
         </div>
         <div className="core-team-member t-three">
           <img src={Tiff} alt="core-team" className="img-core-team" />
           <h2 className="names t-three-p">Raheem Olalekan Usman</h2>
           <p className="position t-three-p">Advisor and Curriculum Developer</p>
-          <div className="links"></div>
+          <div className="links">
+            <i className="fab fa-linkedin icon"></i>
+            <i className="fab fa-twitter icon"></i>
+            <i className="fab fa-instagram icon"></i>
+          </div>
         </div>
         <div className="next">
           <img className="next" src={Back} alt="next" onClick={changeMove} />
