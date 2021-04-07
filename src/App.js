64195ba6835f2a138bac7fd11043620ab3a446f9
SOG-web <raheemolalekanusman84@gmail.com>
2021-04-07 15:29:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Nav from "./components/nav/nav.js";
 import Footer from "./components/footer/Footer.js";
 import AboutPage from "./components/About/About";
